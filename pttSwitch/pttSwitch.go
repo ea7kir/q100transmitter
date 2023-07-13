901f7242bd40a2de8d66f62905b663eafe3495a4
ea7kir <mikenaylorspain@icloud.com>
2023-07-13 20:28:15 +0200

    230713.2028

--- pttSwitch/pttSwitch.go
+++ pttSwitch/pttSwitch.go
@@ -15,53 +15,53 @@ import (
 const (
 	// Analog Devices HVC349 RF Switch
 	// using RFC as the input, RF2 as the output
-	RF_SWITCH_CTRL_GPIO = rpi.J8p18 // pin 18 GPIO_24
-	RF_SWITCH_EN_GPIO   = rpi.J8p16 // pin 16 GPIO_23 (GND pin 14, pin 17 3.3v)
-	RF_SWITCH_LOW       = 0
-	RF_SWITCH_HIGH      = 1
+	CTRL_PIN = rpi.J8p18 // pin 18 GPIO_24
+	EN_PIN   = rpi.J8p16 // pin 16 GPIO_23 (GND pin 14, pin 17 3.3v)
+	LOW      = 0
+	HIGH     = 1
 )
 
 var (
-	switchControl *gpiod.Line
-	switchEnable  *gpiod.Line
+	hvc349Control *gpiod.Line
+	hvc349Enable  *gpiod.Line
 )
 
 // API
 func Initialize() {
-	switchControlLine, err := gpiod.RequestLine("gpiochip0", RF_SWITCH_CTRL_GPIO, gpiod.AsOutput(0))
+	hvc349ControlLine, err := gpiod.RequestLine("gpiochip0", CTRL_PIN, gpiod.AsOutput(0))
 	if err != nil {
 		panic(err)
 	}
-	switchControl = switchControlLine
-	switchControlLine.SetValue(RF_SWITCH_HIGH)
-	switchEnableLine, err := gpiod.RequestLine("gpiochip0", RF_SWITCH_EN_GPIO, gpiod.AsOutput(0))
+	hvc349Control = hvc349ControlLine
+	hvc349ControlLine.SetValue(HIGH)
+	hvc349EnableLine, err := gpiod.RequestLine("gpiochip0", EN_PIN, gpiod.AsOutput(0))
 	if err != nil {
 		panic(err)
 	}
-	switchEnable = switchEnableLine
-	switchEnable.SetValue(RF_SWITCH_LOW)
+	hvc349Enable = hvc349EnableLine
+	hvc349Enable.SetValue(LOW)
 }
 
 // API
 func Stop() {
 	SetPtt(false)
-	switchControl.SetValue(RF_SWITCH_HIGH)
-	switchEnable.SetValue(RF_SWITCH_HIGH)
+	hvc349Control.SetValue(HIGH)
+	hvc349Enable.SetValue(HIGH)
 
-	switchControl.Reconfigure(gpiod.AsInput)
-	switchControl.Close()
-	switchEnable.Reconfigure(gpiod.AsInput)
-	switchEnable.Close()
+	hvc349Control.Reconfigure(gpiod.AsInput)
+	hvc349Control.Close()
+	hvc349Enable.Reconfigure(gpiod.AsInput)
+	hvc349Enable.Close()
 }
 
 // API
 func SetPtt(tx bool) bool {
 	switch tx {
 	case true:
-		switchControl.SetValue(RF_SWITCH_LOW)
+		hvc349Control.SetValue(LOW)
 		logger.Info.Printf("PTT is %v", "Enabled")
 	case false:
-		switchControl.SetValue(RF_SWITCH_HIGH)
+		hvc349Control.SetValue(HIGH)
 		logger.Info.Printf("PTT is %v", "Disabled")
 	}
 	return tx
@@ -73,39 +73,39 @@ func SetPtt(tx bool) bool {
 
 # Analog Devices HVC349 RF Switch
 # using RFC as the input, RF2 as the output
-RF_SWITCH_EN_GPIO               = 23 # pin 16 GPIO_23 (GND pin 14, pin 17 3.3v)
-RF_SWITCH_CTRL_GPIO             = 24 # pin 18 GPIO_24
-RF_SWITCH_HIGH                  = 1
-RF_SWITCH_LOW                   = 0
+EN_PIN               = 23 # pin 16 GPIO_23 (GND pin 14, pin 17 3.3v)
+CTRL_PIN             = 24 # pin 18 GPIO_24
+HIGH                  = 1
+LOW                   = 0
 
 _rf_pi = None
 
 # HMC349 using RFC as the input, RF2 as the output
 
-def _switch_HMC349(gpio, state):
+def _hvc349_HMC349(gpio, state):
     _rf_pi.write(gpio, state)
 
 def _config_HMC349(en_gpio, ctrl_gpio):
     _rf_pi.set_mode(ctrl_gpio, pigpio.OUTPUT)
-    _rf_pi.write(ctrl_gpio, RF_SWITCH_HIGH)
+    _rf_pi.write(ctrl_gpio, HIGH)
     _rf_pi.set_mode(en_gpio, pigpio.OUTPUT)
-    _rf_pi.write(en_gpio, RF_SWITCH_LOW)
+    _rf_pi.write(en_gpio, LOW)
 
-def configure_rf_switches(pi):
+def configure_rf_hvc349es(pi):
     global _rf_pi
     _rf_pi = pi
-    _config_HMC349(RF_SWITCH_EN_GPIO, RF_SWITCH_CTRL_GPIO)
+    _config_HMC349(EN_PIN, CTRL_PIN)
 
-def shutdown_rf_switches():
-    _switch_HMC349(RF_SWITCH_CTRL_GPIO, RF_SWITCH_HIGH)
-    _switch_HMC349(RF_SWITCH_EN_GPIO, RF_SWITCH_HIGH)
+def shutdown_rf_hvc349es():
+    _hvc349_HMC349(CTRL_PIN, HIGH)
+    _hvc349_HMC349(EN_PIN, HIGH)
 
-def switch_rf_switch_On():
+def hvc349_rf_hvc349_On():
     #print("SWITCHING ON RF SWITCH", flush=True)
-    _switch_HMC349(RF_SWITCH_CTRL_GPIO, RF_SWITCH_LOW)
+    _hvc349_HMC349(CTRL_PIN, LOW)
 
-def switch_rf_switch_Off():
+def hvc349_rf_hvc349_Off():
     #print("SWITCHING OFF RF SWITCH", flush=True)
-    _switch_HMC349(RF_SWITCH_CTRL_GPIO, RF_SWITCH_HIGH)
+    _hvc349_HMC349(CTRL_PIN, HIGH)
 
 */
