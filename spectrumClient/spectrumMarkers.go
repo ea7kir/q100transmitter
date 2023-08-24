747a8cfce1df6597e99b961f6b62f2e09e91f5fc
ea7kir <mikenaylorspain@icloud.com>
2023-08-24 17:27:06 +0200

    230824.1727

--- spectrumClient/spectrumMarkers.go
+++ spectrumClient/spectrumMarkers.go
@@ -5,42 +5,44 @@
 
 package spectrumClient
 
-const (
-	divisor = float32(9.18) // TODO: calculatee a mathematical value
-)
+/*****************************************************************
+* SPECTRUM & CALIBRARTION MARKERS
+*****************************************************************/
 
 var (
-	frequencyCentre = map[string]float32{
-		"2403.25 / 01": 230 / divisor,
-		"2403.50 / 02": 256 / divisor,
-		"2403.75 / 03": 281 / divisor,
-		"2404.00 / 04": 307 / divisor,
-		"2404.25 / 05": 332 / divisor,
-		"2404.50 / 06": 358 / divisor,
-		"2404.75 / 07": 383 / divisor,
-		"2405.00 / 08": 409 / divisor,
-		"2405.25 / 09": 434 / divisor,
-		"2405.50 / 10": 460 / divisor,
-		"2405.75 / 11": 485 / divisor,
-		"2406.00 / 12": 511 / divisor,
-		"2406.25 / 13": 536 / divisor,
-		"2406.50 / 14": 562 / divisor,
-		"2406.75 / 15": 588 / divisor,
-		"2407.00 / 16": 613 / divisor,
-		"2407.25 / 17": 639 / divisor,
-		"2407.50 / 18": 664 / divisor,
-		"2407.75 / 19": 690 / divisor,
-		"2408.00 / 20": 715 / divisor,
-		"2408.25 / 21": 741 / divisor,
-		"2408.50 / 22": 767 / divisor,
-		"2408.75 / 23": 792 / divisor,
-		"2409.00 / 24": 818 / divisor,
-		"2409.25 / 25": 843 / divisor,
-		"2409.50 / 26": 869 / divisor,
-		"2409.75 / 27": 894 / divisor,
+	// TODO: calculatee a mathematical values
+	const_frequencyCentre = map[string]float32{
+		"2403.25 / 01": 230, // / divisor,
+		"2403.50 / 02": 256, // / divisor,
+		"2403.75 / 03": 281, // / divisor,
+		"2404.00 / 04": 307, // / divisor,
+		"2404.25 / 05": 332, // / divisor,
+		"2404.50 / 06": 358, // / divisor,
+		"2404.75 / 07": 383, // / divisor,
+		"2405.00 / 08": 409, // / divisor,
+		"2405.25 / 09": 434, // / divisor,
+		"2405.50 / 10": 460, // / divisor,
+		"2405.75 / 11": 485, // / divisor,
+		"2406.00 / 12": 511, // / divisor,
+		"2406.25 / 13": 536, // / divisor,
+		"2406.50 / 14": 562, // / divisor,
+		"2406.75 / 15": 588, // / divisor,
+		"2407.00 / 16": 613, // / divisor,
+		"2407.25 / 17": 639, // / divisor,
+		"2407.50 / 18": 664, // / divisor,
+		"2407.75 / 19": 690, // / divisor,
+		"2408.00 / 20": 715, // / divisor,
+		"2408.25 / 21": 741, // / divisor,
+		"2408.50 / 22": 767, // / divisor,
+		"2408.75 / 23": 792, // / divisor,
+		"2409.00 / 24": 818, // / divisor,
+		"2409.25 / 25": 843, // / divisor,
+		"2409.50 / 26": 869, // / divisor,
+		"2409.75 / 27": 894, // / divisor,
 	}
 
-	symbolRateWidth = map[string]float32{ // TODO: calculatee a mathematical values
+	// TODO: calculatee a mathematical values
+	const_symbolRateWidth = map[string]float32{
 		"2000": 20,
 		"1500": 15,
 		"1000": 10,
@@ -54,9 +56,9 @@ var (
 )
 
 // Returns frequency and bandWidth Markers as float32
-func getMarkers(frequency, sysmbolRate string) (float32, float32) {
-	centre := frequencyCentre[frequency]
-	width := symbolRateWidth[sysmbolRate]
+func getMarkers(frequency, symbolRate string) (float32, float32) {
+	centre := const_frequencyCentre[frequency] / 9.18 // 9.18 is a temporary kludge
+	width := const_symbolRateWidth[symbolRate]
 	return centre, width
 }
 
