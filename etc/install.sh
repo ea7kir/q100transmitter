712fdfd125cab9d569ae09e72217a1f63ceb4440
Michael Naylor <84274919+ea7kir@users.noreply.github.com>
2024-08-17 13:31:49 +0200

    Update install.sh

--- etc/install.sh
+++ etc/install.sh
@@ -215,17 +215,20 @@ Bring up connections in manusl
 "
 
 # lan
+sudo nmcli con down Wired\ connection\ 1
 sudo nmcli con mod Wired\ connection\ 1 ipv4.addresses 192.168.1.150/24
 sudo nmcli con mod Wired\ connection\ 1 ipv4.gateway 192.168.1.1
 sudo nmcli con mod Wired\ connection\ 1 ipv4.method manual
 sudo nmcli con up Wired\ connection\ 1
 
 # pluto
+sudo nmcli con down Wired\ connection\ 1
 sudo nmcli con mod Wired\ connection\ 2 ipv4.addresses 192.168.2.10/24
 #sudo nmcli con mod Wired\ connection\ 2 ipv4.gateway 192.168.2.0
 sudo nmcli con mod Wired\ connection\ 2 ipv4.method manual
 sudo nmcli con up Wired\ connection\ 2
 # encoder
+sudo nmcli con down Wired\ connection\ 3
 sudo nmcli con mod Wired\ connection\ 3 ipv4.addresses 192.168.3.10/24
 #sudo nmcli con mod Wired\ connection\ 3 ipv4.gateway 192.168.3.0
 sudo nmcli con mod Wired\ connection\ 3 ipv4.method manual
